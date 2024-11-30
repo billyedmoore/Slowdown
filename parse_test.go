2e833906402a302338d96177fd08d1d72e3d7c6c
Billy Moore <billyedmoore@yahoo.com>
2024-11-30 14:16:12 +0000

    Added some tests for basic inline parsing.

--- parse_test.go
+++ parse_test.go
@@ -25,3 +25,32 @@ func TestEmptyFileParsing(t *testing.T) {
 		t.Fatalf("Should be 0 children instead found %d", len(root.GetChildren()))
 	}
 }
+
+func TestAllInlineBlocksBeingParsed(t *testing.T) {
+	lines := []string{"#Top Level", "Line one", "Line two", "", "Second paragraph"}
+	root := parser.Parse(lines)
+
+	var queue []parser.Node = make([]parser.Node, 0)
+
+	queue = append(queue, root)
+
+	var current_node parser.Node = nil
+	for len(queue) > 0 {
+		current_node, queue = queue[0], queue[1:]
+		queue = append(queue, current_node.GetChildren()...)
+		if current_node.GetNodeType() == "UNPARSED_INLINE" {
+			t.Fatalf("Found an unparsed inline block after parsing")
+		}
+	}
+}
+
+func TestRawTextInParagraph(t *testing.T) {
+	lines := []string{"Line one", "", "Second paragraph"}
+	root := parser.Parse(lines)
+	// should be root.paragraph.rawText
+	firstContent := root.GetChildren()[0].GetChildren()[0].GetContent()
+
+	if firstContent != "Line one" {
+		t.Fatalf("Expected \"Line one\", instead got \"%v\"", firstContent)
+	}
+}
